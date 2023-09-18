39bfcb7cc4a3a7a344d7a3bcbee7b3e8c92c9ad9
Taimoor Arshad <taimoorjassal@gmail.com>
2023-09-18 15:09:56 +0500

    Update streamlit_app.py
    Added styling to the fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalizes the dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays the normalized data
+streamlit.dataframe(fruityvice_normalized)
