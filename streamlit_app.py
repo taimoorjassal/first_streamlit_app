0583cec119e146d1df37985543014ae142ecffcb
Taimoor Arshad <taimoorjassal@gmail.com>
2023-09-18 15:08:11 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 # 1st Section
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 4 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and ROcket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocada Toast')
