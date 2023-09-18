94144ba7e79f7a9fdf799ba9d8e81be9cbc92e96
Taimoor Arshad <taimoorjassal@gmail.com>
2023-09-18 11:08:44 +0500

    Update streamlit_app.py
    fixed more typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title("My Parents new Healthy Diner")
-streamlit.header("Breakfast Menu")
+streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
 
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and ROcket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and ROcket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
