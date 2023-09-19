64a45055c8a4586c2bf4d3277d9de27343e9d61f
Taimoor Arshad <taimoorjassal@gmail.com>
2023-09-19 12:55:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,16 +46,16 @@ streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])    
     my_data_rows = my_cur.fetchall()
     return my_data_rows
 #my_cur = my_cnx.cursor()
 # Allow the user to add a fruit
 if streamlit.button('Get Fruit List'):
-  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
   my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+  
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values('" + new_fruit +"');")
@@ -65,6 +65,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfru
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.xlose()
   streamlit.text(back_from_function)
   #my_cur.execute("SELECT * from fruit_load_list")
 
